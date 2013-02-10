9464db44030f192b5a9df2ae7f2fc83c09b52469
Bob Nadler <bnadlerjr@gmail.com>
2013-02-10 01:38:49 -0500

    Custom AnnotationView with stub for image detail button.

--- app/controllers/map_view_controller.rb
+++ app/controllers/map_view_controller.rb
@@ -1,23 +1,52 @@
 class MapViewController < CyrusSnapsViewController
   def viewDidLoad
     super
-    annotateMapView
-    self.view.addSubview(mapView)
+    annotatePhotoMapView
+    self.view.addSubview(photoMapView)
+  end
+
+  def mapView(mapView, viewForAnnotation: annotation)
+    return nil unless annotation.is_a?(PhotoAnnotation)
+    dequeueOrInitAnnotationView(mapView, annotation)
+  end
+
+  def showImage(sender)
+    puts "Show IMAGE!!!"
   end
 
   private
 
-  def mapView
-    @mapView ||= MKMapView.alloc.initWithFrame(self.view.bounds).tap do |map|
-      map.mapType = MKMapTypeStandard
-      map.showsUserLocation = true
-      map.userTrackingMode = MKUserTrackingModeFollow
+  ANNOTATION_VIEW_REUSE_ID = "AnnotationView"
+
+  def dequeueOrInitAnnotationView(mapView, annotation)
+    mapView.dequeueReusableAnnotationViewWithIdentifier(ANNOTATION_VIEW_REUSE_ID) ||
+      MKPinAnnotationView.alloc.initWithAnnotation(
+        annotation, reuseIdentifier: ANNOTATION_VIEW_REUSE_ID).tap do |view|
+
+      view.canShowCallout = true
+      view.rightCalloutAccessoryView = detailsButton
+      end
+  end
+
+  def detailsButton
+    @detailsButton ||= UIButton.buttonWithType(UIButtonTypeDetailDisclosure).tap do |b|
+      b.addTarget(self, action: :'showImage:', forControlEvents: UIControlEventTouchUpInside)
     end
   end
 
-  def annotateMapView
+  def photoMapView
+    @photoMapView ||= MKMapView.alloc.init.tap do |map|
+      map.frame = self.view.bounds
+      map.mapType = MKMapTypeStandard
+      map.showsUserLocation = true
+      map.userTrackingMode = MKUserTrackingModeFollow
+      map.delegate = self
+    end
+  end
+
+  def annotatePhotoMapView
     Photo.find_all do |photo|
-      mapView.addAnnotation(PhotoAnnotation.alloc.initWithPhoto(photo))
+      photoMapView.addAnnotation(PhotoAnnotation.alloc.initWithPhoto(photo))
     end
   end
 end
