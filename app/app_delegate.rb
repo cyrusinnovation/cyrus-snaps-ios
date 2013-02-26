da3e7ca37de958daca51989642e526b23a9287be
Bob Nadler <bnadlerjr@gmail.com>
2013-02-25 22:50:27 -0500

    Splash screen (closes #11)

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,8 +4,36 @@ class AppDelegate
       w.frame = UIScreen.mainScreen.bounds
       w.makeKeyAndVisible
       w.rootViewController = MenuViewController.alloc.init
+
+      # show splash screen
+      w.rootViewController.view.addSubview(splash_image_view)
+      w.rootViewController.view.bringSubviewToFront(splash_image_view)
     end
 
+    fade_out_splash_screen
+
     true
+  end
+
+  private
+
+  def splash_image_view
+    @splash_image_view ||=
+      UIImageView.alloc.initWithImage(UIImage.imageNamed("Default.png"))
+  end
+
+  def fade_out_splash_screen
+    fade_out_timer = 5.0
+    UIView.transitionWithView(
+      @window,
+      duration: fade_out_timer,
+      options: UIViewAnimationOptionTransitionNone,
+      animations: lambda { splash_image_view.alpha = 0 },
+      completion: lambda do |finished|
+        splash_image_view.removeFromSuperview
+        splash_image_view = nil
+        UIApplication.sharedApplication.setStatusBarHidden(false, animated: false)
+      end
+    )
   end
 end
