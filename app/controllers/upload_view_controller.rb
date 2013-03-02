6fbe9dfa6be4b3bd0c42c0e9720d3e9159cc2c92
Bob Nadler <bnadlerjr@gmail.com>
2013-03-02 13:32:39 -0500

    Style upload page (closes #22).

--- app/controllers/upload_view_controller.rb
+++ app/controllers/upload_view_controller.rb
@@ -2,7 +2,7 @@ class UploadViewController < CyrusSnapsViewController
   def viewDidLoad
     super
     self.navigationItem.title = 'Upload'
-    self.view.backgroundColor = UIColor.whiteColor
+    self.view.backgroundColor = UIColor.colorWithRed(115/255.0, green: 115/255.0, blue: 35/255.0, alpha: 1.0)
     self.view.addSubview(titleTextField)
     self.view.addSubview(chooseButton)
     self.view.addSubview(uploadButton)
@@ -42,10 +42,13 @@ class UploadViewController < CyrusSnapsViewController
     @titleTextField ||= UITextField.alloc.init.tap do |field|
       field.autocapitalizationType = UITextAutocapitalizationTypeWords
       field.borderStyle = UITextBorderStyleRoundedRect
+      field.textAlignment = NSTextAlignmentCenter
+      field.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter
       field.delegate = self
       field.enablesReturnKeyAutomatically = true
-      field.frame = [[10, 10], [self.view.frame.size.width - 20, 25]]
-      field.placeholder = "Title"
+      field.sizeToFit
+      field.frame = [[10, 10], [self.view.frame.size.width - 20, field.frame.size.height + 5]]
+      field.placeholder = "Enter a Title for Your Photo"
       field.returnKeyType = UIReturnKeyDone
     end
   end
@@ -54,25 +57,37 @@ class UploadViewController < CyrusSnapsViewController
     @imageView ||= UIImageView.alloc.init.tap do |view|
       x = self.view.frame.size.width - 20
       y = self.view.frame.size.height - 150
+      view.layer.cornerRadius = 8;
+      view.layer.shadowOpacity = 0.1
       view.setFrame([[10, 50], [x, y]])
       view.backgroundColor = UIColor.whiteColor
     end
   end
 
   def chooseButton
-    @chooseButton ||= UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
+    @chooseButton ||= UIButton.buttonWithType(UIButtonTypeCustom).tap do |b|
       b.setTitle("Choose Photo", forState: UIControlStateNormal)
+      b.setBackgroundColor(UIColor.colorWithRed(190/255.0, green: 202/255.0, blue: 46/255.0, alpha: 1.0))
+      b.layer.cornerRadius = 8;
+      b.layer.borderWidth = 1;
       b.sizeToFit
-      b.frame = CGRect.new([10, 365], b.frame.size)
+      height = b.frame.size.height + 20
+      width = b.frame.size.width + 20
+      b.frame = CGRect.new([10, 365], [width, height])
       b.addTarget(self, action: :'choosePicture:', forControlEvents: UIControlEventTouchUpInside)
     end
   end
 
   def uploadButton
-    @uploadButton ||= UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
+    @uploadButton ||= UIButton.buttonWithType(UIButtonTypeCustom).tap do |b|
       b.setTitle("Upload", forState: UIControlStateNormal)
+      b.setBackgroundColor(UIColor.colorWithRed(190/255.0, green: 202/255.0, blue: 46/255.0, alpha: 1.0))
+      b.layer.cornerRadius = 8;
+      b.layer.borderWidth = 1;
       b.sizeToFit
-      b.frame = CGRect.new([200, 365], b.frame.size)
+      height = b.frame.size.height + 20
+      width = b.frame.size.width + 20
+      b.frame = CGRect.new([200, 365], [width, height])
       b.addTarget(self, action: :'uploadPhoto:', forControlEvents: UIControlEventTouchUpInside)
     end
   end
