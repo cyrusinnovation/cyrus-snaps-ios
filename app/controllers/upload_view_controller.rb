6f939e3755f1503b8fdc4856e853c50a6d380109
Bob Nadler <bnadlerjr@gmail.com>
2013-03-02 18:16:56 -0500

    Change how photos are chosen for upload.
    Make UIImageView on Upload screen clickable; remove "Choose Photo" button.

--- app/controllers/upload_view_controller.rb
+++ app/controllers/upload_view_controller.rb
@@ -8,7 +8,6 @@ class UploadViewController < CyrusSnapsViewController
     self.view.backgroundColor = UIColor.colorWithRed(115/255.0, green: 115/255.0, blue: 35/255.0, alpha: 1.0)
 
     self.view.addSubview(titleTextField)
-    self.view.addSubview(chooseButton)
     self.view.addSubview(imageView)
   end
 
@@ -31,6 +30,7 @@ class UploadViewController < CyrusSnapsViewController
 
   def imagePickerController(picker, didFinishPickingMediaWithInfo: info)
     imageView.image = info.objectForKey(UIImagePickerControllerOriginalImage)
+    clickHereLabel.removeFromSuperview
     self.dismissViewControllerAnimated(true, completion: nil)
   end
 
@@ -59,25 +59,25 @@ class UploadViewController < CyrusSnapsViewController
   def imageView
     @imageView ||= UIImageView.alloc.init.tap do |view|
       x = self.view.frame.size.width - 20
-      y = self.view.frame.size.height - 150
+      y = self.view.frame.size.height - 120
       view.layer.cornerRadius = 8;
       view.layer.shadowOpacity = 0.1
-      view.setFrame([[10, 50], [x, y]])
+      view.setFrame([[10, 60], [x, y]])
       view.backgroundColor = UIColor.whiteColor
+      view.userInteractionEnabled = true
+      view.addGestureRecognizer(UITapGestureRecognizer.alloc.initWithTarget(self, action: :'choosePicture:'))
+      view.addSubview(clickHereLabel)
     end
   end
 
-  def chooseButton
-    @chooseButton ||= UIButton.buttonWithType(UIButtonTypeCustom).tap do |b|
-      b.setTitle("Choose Photo", forState: UIControlStateNormal)
-      b.setBackgroundColor(UIColor.colorWithRed(190/255.0, green: 202/255.0, blue: 46/255.0, alpha: 1.0))
-      b.layer.cornerRadius = 8;
-      b.layer.borderWidth = 1;
-      b.sizeToFit
-      height = b.frame.size.height + 20
-      width = b.frame.size.width + 20
-      b.frame = CGRect.new([10, 365], [width, height])
-      b.addTarget(self, action: :'choosePicture:', forControlEvents: UIControlEventTouchUpInside)
+  def clickHereLabel
+    @clickHereLabel ||= UILabel.alloc.init.tap do |label|
+      label.font = label.font.fontWithSize(36)
+      label.adjustsFontSizeToFitWidth = true
+      label.frame = [[20, 10], [self.view.frame.size.width - 60, 50]]
+      label.textColor = UIColor.grayColor
+      label.text = "Tap to choose photo"
+      label.textAlignment = UITextAlignmentCenter
     end
   end
 
